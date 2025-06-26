331f4d187aed96f91a3301dfef1b1762d91bb33c
Komal Kalshetti <komalkalshetti989@gmail.com>
2025-06-26 19:58:39 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -93,10 +93,14 @@ app.get('/', (req, res) => {
 });
 
 // Serve static assets in production
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
+
 
 
 // Server configuration
